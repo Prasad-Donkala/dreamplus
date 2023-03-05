7d8536ec5917cf861ed3d53397104121c0faff2a
prasad <you@example.com>
2023-03-05 10:58:03 +0530

    sample merge

--- sample.java
+++ sample.java
@@ -1,9 +1,9 @@
-  public class login
+  public class Login
   {
       public static void main ()
       {
           int i=0;
-	  for (i=0;i<=20;i++)
+	  for (i=0;i<=40;i++)
 	  {
 		System.out.Println ("Number: "+i);
       	  }
